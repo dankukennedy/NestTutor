7c7ce844d94d5e461c3695559b7d091f428578ea
dankukennedy <dankukennedy@users.noreply.github.com>
2025-12-25 23:56:15 +0000

    coreection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+   */
+  @Get()  //GET /users
+  findAll(){
+    return []
+  }
+
+  @Get(':id') //GET /users/:id
+  findOne(@Param('id') id: string){
+   return {id}
+  }
+
+  @Post() //POST /users
+  create(@Body() users: {}){
+    return users
+  }
+
+  @Patch(':id') //PATCH /users/:id
+   update(@Param('id') id: string,@Body() userUpdate:{}){
+    return {id, ...userUpdate}
+   }
+
+}
