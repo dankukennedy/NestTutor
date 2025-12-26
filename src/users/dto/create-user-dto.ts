40403f2ab5af62ed9ea4a194473875aeb80145c3
dankukennedy <dankukennedy@users.noreply.github.com>
2025-12-26 02:09:04 +0000

    vim

--- src/users/dto/create-user-dto.ts
+++ src/users/dto/create-user-dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+    @IsEmail()      
     email: string;
+    @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], { message: 'role must be INTERN, ENGINEER, or ADMIN' })
     role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
